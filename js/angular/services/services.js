b0a8b12b1e22781a6c56381e5c754fddf25f896d
blackwires <a.albertini2b@gmail.com>
2015-03-09 01:28:16 -0400

    [CLEAN] reworking services

--- js/angular/services/services.js
+++ js/angular/services/services.js
@@ -74,209 +74,208 @@
             this.isHover = false;
         }
 
-        return {
-            getTitle               : function ()
-            {
-                return title;
-            },
-            setTitle               : function (value)
-            {
-                title = value;
-            },
-            getHomeArtists         : function ()
-            {
-                return homeArtists;
-            },
-            setHomeArtists         : function (artists)
-            {
-                homeArtists = [];
-                angular.copy(artists, homeArtists);
-            },
-            getHomeAlbums          : function ()
-            {
-                return homeAlbums;
-            },
-            setHomeAlbums          : function (albums)
-            {
-                homeAlbums = [];
-                angular.copy(albums, homeAlbums);
-            },
-            getPlaylists           : function ()
-            {
-                if (playlists)
-                {
-                    for (var i = 0; i < playlists.length; ++i)
-                    {
-                        playlists[i].id = i;
-                    }
-                }
-                return playlists;
-            },
-            setPlaylists           : function (value)
-            {
-                playlists = [];
-                angular.copy(playlists, value);
-                localStorageService.set('playlists', playlists);
-            },
-            getPlaylist            : function (id)
+
+        this.getTitle = function ()
+        {
+            return title;
+        }
+        this.setTitle = function (value)
+        {
+            title = value;
+        }
+        this.getHomeArtists = function ()
+        {
+            return homeArtists;
+        }
+        this.setHomeArtists = function (artists)
+        {
+            homeArtists = [];
+            angular.copy(artists, homeArtists);
+        }
+        this.getHomeAlbums = function ()
+        {
+            return homeAlbums;
+        }
+        this.setHomeAlbums = function (albums)
+        {
+            homeAlbums = [];
+            angular.copy(albums, homeAlbums);
+        }
+        this.getPlaylists = function ()
+        {
+            if (playlists)
             {
                 for (var i = 0; i < playlists.length; ++i)
                 {
                     playlists[i].id = i;
-                    if (i == id)
-                    {
-                        return playlists[i];
-                    }
                 }
-                return null;
-            },
-            getTrackFromPlaylist   : function (trackId, playlistId)
+            }
+            return playlists;
+        }
+        this.setPlaylists = function (value)
+        {
+            playlists = [];
+            angular.copy(playlists, value);
+            localStorageService.set('playlists', playlists);
+        }
+        this.getPlaylist = function (id)
+        {
+            for (var i = 0; i < playlists.length; ++i)
             {
-                for (var i = 0; i < playlists.length; ++i)
+                playlists[i].id = i;
+                if (i == id)
                 {
-                    if (playlists[i].id == id)
+                    return playlists[i];
+                }
+            }
+            return null;
+        }
+        this.getTrackFromPlaylist = function (trackId, playlistId)
+        {
+            for (var i = 0; i < playlists.length; ++i)
+            {
+                if (playlists[i].id == id)
+                {
+                    var playlist = playlists[i];
+                    for (var j = 0; j < playlist.tracks.length; ++j)
                     {
-                        var playlist = playlists[i];
-                        for (var j = 0; j < playlist.tracks.length; ++j)
+                        if (playlist.tracks[j].idInPlaylist == trackId)
                         {
-                            if (playlist.tracks[j].idInPlaylist == trackId)
-                            {
-                                return playlist.tracks[j];
-                            }
+                            return playlist.tracks[j];
                         }
                     }
                 }
-                return null;
-            },
-            addTrackToPlaylist     : function (track, playlistId)
+            }
+            return null;
+        }
+        this.addTrackToPlaylist = function (track, playlistId)
+        {
+            for (var i = 0; i < playlists.length; ++i)
+            {
+                if (i == playlistId)
+                {
+                    var playlist = playlists[i];
+                    var tracksLength = playlist.tracks.length;
+                    //for (var j = 0; j < tracksLength; ++j)
+                    //{
+                    //    if (playlist.tracks[j].idInPlaylist == track.id)
+                    //    {
+                    //        return false;
+                    //    }
+                    //}
+                    track.idInPlaylist = tracksLength;
+                    playlist.tracks[tracksLength] = track;
+                    localStorageService.set('playlists', playlists);
+                    return true;
+                }
+            }
+            return false;
+        }
+        this.addTrackArrayToPlaylist = function (tracks, playlistId)
+        {
+            if (tracks)
             {
                 for (var i = 0; i < playlists.length; ++i)
                 {
                     if (i == playlistId)
                     {
                         var playlist = playlists[i];
-                        var tracksLength = playlist.tracks.length;
-                        //for (var j = 0; j < tracksLength; ++j)
-                        //{
-                        //    if (playlist.tracks[j].idInPlaylist == track.id)
-                        //    {
-                        //        return false;
-                        //    }
-                        //}
-                        track.idInPlaylist = tracksLength;
-                        playlist.tracks[tracksLength] = track;
+                        playlist.tracks = playlist.tracks.concat(tracks);
                         localStorageService.set('playlists', playlists);
                         return true;
                     }
                 }
-                return false;
-            },
-            addTrackArrayToPlaylist: function (tracks, playlistId)
+            }
+            return false;
+        }
+        this.removeTrackFromPlaylist = function (trackIdInPlaylist, playlistId)
+        {
+            for (var i = 0; i < playlists.length; ++i)
             {
-                if (tracks)
+                if (i == playlistId)
                 {
-                    for (var i = 0; i < playlists.length; ++i)
+                    var playlist = playlists[playlistId];
+                    for (var j = 0; j < playlist.tracks.length; ++j)
                     {
-                        if (i == playlistId)
+                        if (j == trackIdInPlaylist)
                         {
-                            var playlist = playlists[i];
-                            playlist.tracks = playlist.tracks.concat(tracks);
+                            playlist.tracks.splice(j, 1);
                             localStorageService.set('playlists', playlists);
                             return true;
                         }
                     }
                 }
-                return false;
-            },
-            removeTrackFromPlaylist: function (trackIdInPlaylist, playlistId)
+            }
+            return false;
+        }
+        this.createPlaylist = function (name)
+        {
+            if (name)
+            {
+                var playlistLength = playlists.length;
+                var newPlaylist = new Playlist();
+
+                newPlaylist.name = name;
+                newPlaylist.id = playlistLength;
+                newPlaylist.tracks = [];
+                playlists[playlistLength] = newPlaylist;
+                localStorageService.set('playlists', playlists);
+
+                return newPlaylist;
+            }
+            return null;
+        }
+        this.addExistingPlaylist = function (playlist)
+        {
+            var playlistLength = playlists.length;
+            playlists[playlistLength] = playlist;
+            localStorageService.set('playlists', playlists);
+        }
+        this.removePlaylist = function (id)
+        {
+            playlists.splice(id, 1);
+            localStorageService.set('playlists', playlists);
+        }
+        this.renamePlaylist = function (id, newName)
+        {
+            if (newName)
             {
                 for (var i = 0; i < playlists.length; ++i)
                 {
-                    if (i == playlistId)
+                    if (i == id)
                     {
-                        var playlist = playlists[playlistId];
-                        for (var j = 0; j < playlist.tracks.length; ++j)
-                        {
-                            if (j == trackIdInPlaylist)
-                            {
-                                playlist.tracks.splice(j, 1);
-                                localStorageService.set('playlists', playlists);
-                                return true;
-                            }
-                        }
+                        var playlist = playlists[id];
+                        playlist.name = newName;
+                        console.log("Rename: " + playlist.name);
+                        localStorageService.set('playlists', playlists);
+                        return true;
                     }
                 }
-                return false;
-            },
-            createPlaylist         : function (name)
-            {
-                if (name)
-                {
-                    var playlistLength = playlists.length;
-                    var newPlaylist = new Playlist();
-
-                    newPlaylist.name = name;
-                    newPlaylist.id = playlistLength;
-                    newPlaylist.tracks = [];
-                    playlists[playlistLength] = newPlaylist;
-                    localStorageService.set('playlists', playlists);
-
-                    return newPlaylist;
-                }
-                return null;
-            },
-            addExistingPlaylist    : function (playlist)
-            {
-                var playlistLength = playlists.length;
-                playlists[playlistLength] = playlist;
-                localStorageService.set('playlists', playlists);
-            },
-            removePlaylist         : function (id)
-            {
-                playlists.splice(id, 1);
-                localStorageService.set('playlists', playlists);
-            },
-            renamePlaylist         : function (id, newName)
-            {
-                if (newName)
-                {
-                    for (var i = 0; i < playlists.length; ++i)
-                    {
-                        if (i == id)
-                        {
-                            var playlist = playlists[id];
-                            playlist.name = newName;
-                            console.log("Rename: " + playlist.name);
-                            localStorageService.set('playlists', playlists);
-                            return true;
-                        }
-                    }
-                }
-                return false;
-            },
-            getCurrentTrack        : function ()
-            {
-                return currentTrack;
-                updateTrackStates();
-            },
-            setCurrentTrack        : function (track, state)
-            {
-                currentTrack = track;
-                if (state)
-                {
-                    currentTrack.playState = state;
-                }
-                updateTrackStates();
-            },
-            getPlayStates          : function ()
-            {
-                return playStates
-            },
-            updateTrackStates      : function ()
-            {
-                updateTrackStates();
             }
-        };
+            return false;
+        }
+        this.getCurrentTrack = function ()
+        {
+            return currentTrack;
+            updateTrackStates();
+        }
+        this.setCurrentTrack = function (track, state)
+        {
+            currentTrack = track;
+            if (state)
+            {
+                currentTrack.playState = state;
+            }
+            updateTrackStates();
+        }
+        this.getPlayStates = function ()
+        {
+            return playStates
+        }
+        this.updateTrackStates = function ()
+        {
+            updateTrackStates();
+        }
     });
 
     services.service('sharedPagesStatus', function ($location)
@@ -297,12 +296,12 @@
             pageTitle = value;
         }
 
-        this.getErrorMessage = function()
+        this.getErrorMessage = function ()
         {
             return errorMessage;
         }
 
-        this.setErrorMessage = function(msg)
+        this.setErrorMessage = function (msg)
         {
             errorMessage = msg;
         }
@@ -333,7 +332,7 @@
             isPageLoaded = false;
         }
 
-        this.pageFailedLoad = function()
+        this.pageFailedLoad = function ()
         {
             this.setIsPageError(true);
             this.setIsPageLoaded(true);
