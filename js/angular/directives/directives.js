be5a0eabf5e681483ee92297394a6feb792863ef
blackwires <a.albertini2b@gmail.com>
2015-03-08 23:32:22 -0400

    [RELEASE] 2.02 - Fix images sizes

--- js/angular/directives/directives.js
+++ js/angular/directives/directives.js
@@ -58,4 +58,15 @@
             templateUrl: './views/templates/notfound.html'
         };
     });
+
+    ubeatApp.directive('backImg', function(){
+        return function(scope, element, attrs){
+            attrs.$observe('backImg', function(value) {
+                element.css({
+                    'background-image': 'url(' + value +')',
+                    'background-size' : 'cover'
+                });
+            });
+        };
+    });
 })();
