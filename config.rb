6d1d86044977ddf61d5ee2652129e7ee74fd6c08
miramo <maxime.miramond@epitech.eu>
2015-03-12 16:20:10 -0400

    [DEL] slick.js bower [UPD] bower

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
 add_import_path "bower_components/foundation/scss"
 add_import_path "bower_components/foundation-icon-fonts"
-add_import_path "bower_components/slick.js/slick"
+add_import_path "bower_components/slick-carousel/slick"
 add_import_path "bower_components/fontawesome/scss"
 add_import_path "bower_components/material-design-iconic-font/scss"
 
